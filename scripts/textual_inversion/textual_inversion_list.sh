0603b1adbb05fb82e17050142f91188ec66af559
Guocheng Qian <guocheng.qian@outlook.com>
2023-08-15 18:33:09 +0000

    update results

--- scripts/textual_inversion/textual_inversion_list.sh
+++ scripts/textual_inversion/textual_inversion_list.sh
@@ -1,13 +1,22 @@
 
 
-examples=(
-	'data/nerf4/chair'
-	'data/nerf4/drums'
-	'data/nerf4/ficus'
-	'data/nerf4/mic'
-)
+# examples=(
+# 	'data/nerf4/chair'
+# 	'data/nerf4/drums'
+# 	'data/nerf4/ficus'
+# 	'data/nerf4/mic'
+# )
 
-for i in "${examples[@]}"; do
-	filename=$(basename "$i")
-	bash scripts/texural_inversion/textual_inversion.sh 0 runwayml/stable-diffusion-v1-5 "$i"/rgba.png out/texural_inversion/${filename} _nerf_${filename}_ ${filename} --max_train_steps 3000
-done
+# for i in "${examples[@]}"; do
+# 	filename=$(basename "$i")
+# 	bash scripts/textual_inversion/textual_inversion.sh 0 runwayml/stable-diffusion-v1-5 "$i"/rgba.png out/textual_inversion/${filename} _nerf_${filename}_ ${filename} --max_train_steps 3000
+# done
+
+
+device=$1
+# bash scripts/textual_inversion/textual_inversion.sh $device runwayml/stable-diffusion-v1-5 data/demo/blackpink-lisa/image.png out/textual_inversion/data/demo/blackpink-lisa _blackpink_lisa_ lisa --max_train_steps 3000
+
+# bash scripts/textual_inversion/textual_inversion.sh $device runwayml/stable-diffusion-v1-5 data/demo/blackpink-lisa/image.png out/textual_inversion/data/demo/a-fullbody-ironman _ironman_ ironman --max_train_steps 3000
+
+# bash scripts/textual_inversion/textual_inversion.sh $device runwayml/stable-diffusion-v1-5 data/demo/taylor-swift-sit-1989/image.png out/textual_inversion/data/demo/taylor-swift-sit _taylor_swift_sit_ taylor --max_train_steps 3000
+bash scripts/textual_inversion/textual_inversion.sh $device runwayml/stable-diffusion-v1-5 data/demo/taylor-swift-in-red-hat-blue-t-shift-light-green-skirt-long-black-sock-red-high-heel-shoe-with-a-blue-bag/image.png out/textual_inversion/data/demo/taylor-swift2 _taylor_swift_2_ taylor --max_train_steps 3000
